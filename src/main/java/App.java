36db756f24b89c63c62e671fafb627421b2cce3e
bl <bouchkhaf@gmail.com>
2023-11-14 10:11:26 +0100

    test_step1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,5 +12,30 @@ public class App {
 
     public static void main(String[] args) {
         // A completer
+        // A completer
+        TextBuffer b = new TextBuffer("cours qualite logiciel");
+        EmacsKillRing kc = new EmacsKillRing();
+        TextEditor ed = new TextEditor("Je suis un editeur");
+
+        try {
+            System.out.println(ed.getBuffer());
+
+            ed.setCursor(3);
+            ed.setMark(7);
+            ed.killRingBackup();
+            System.out.println(ed.getBuffer());
+
+            ed.setCursor(11);
+            ed.setMark(16);
+            ed.killSection();
+            System.out.println(ed.getBuffer());
+
+            ed.setCursor(8);
+            ed.yank();
+        } catch (EmacsKillRingOverflowException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
